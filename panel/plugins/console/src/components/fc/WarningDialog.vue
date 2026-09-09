22191e146a321bbd544ccddd54a3f96c35089795
Csituka_D <694428809@qq.com>
2026-09-09 14:02:47 +0800

    样式更新

--- panel/plugins/console/src/components/fc/WarningDialog.vue
+++ panel/plugins/console/src/components/fc/WarningDialog.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import type { MountComponent } from "../../types";
 import { t } from "@/lang/i18n";
+import { VBtn, VCard, VCardActions, VCardText, VCardTitle, VCheckbox, VDialog, VSpacer } from "vuetify/components";
 
-interface Props extends MountComponent {
+interface Props {
   title: string;
   subTitle: string;
   checkText: string;
+  destroyComponent?: (delay?: number) => void;
 }
 
 const props = defineProps<Props>();
@@ -43,42 +44,54 @@ defineExpose({
 </script>
 
 <template>
-  <a-modal
-    v-model:open="open"
-    centered
-    width="600px"
-    title=""
-    :mask-closable="false"
-    :closable="false"
-    :footer="null"
-    @cancel="cancel"
+  <VDialog
+    v-model="open"
+    class="app-dialog warning-dialog"
+    max-width="600"
+    persistent
+    scrollable
   >
-    <div class="dialog-overflow-container">
-      <div class="flex flex-center">
-        <div>
-          <a-typography-paragraph>
-            <a-typography-title :level="4">
-              <div class="flex flex-center mb-24">
-                {{ props.title }}
-              </div>
-            </a-typography-title>
-            <a-typography-text>
-              <div class="flex flex-center" style="gap: 10px">
-                <pre>{{ props.subTitle }}</pre>
-              </div>
-            </a-typography-text>
-          </a-typography-paragraph>
-          <div class="justify-center mb-24">
-            <a-checkbox v-model:checked="checkbox">{{ props.checkText }}</a-checkbox>
-          </div>
-          <div class="flex justify-center">
-            <a-button class="mr-12" danger :disabled="!checkbox" @click="confirm">
-              {{ t("TXT_CODE_d507abff") }}
-            </a-button>
-            <a-button @click="cancel">{{ t("TXT_CODE_a0451c97") }}</a-button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </a-modal>
+    <VCard rounded="xl">
+      <VCardTitle class="warning-dialog__title">{{ props.title }}</VCardTitle>
+      <VCardText class="warning-dialog__body">
+        <pre class="warning-dialog__message">{{ props.subTitle }}</pre>
+        <VCheckbox v-model="checkbox" :label="props.checkText" hide-details density="comfortable" />
+      </VCardText>
+      <VCardActions class="warning-dialog__actions">
+        <VSpacer />
+        <VBtn color="error" variant="text" rounded="xl" :disabled="!checkbox" @click="confirm">
+          {{ t("TXT_CODE_d507abff") }}
+        </VBtn>
+        <VBtn variant="text" rounded="xl" @click="cancel">
+          {{ t("TXT_CODE_a0451c97") }}
+        </VBtn>
+      </VCardActions>
+    </VCard>
+  </VDialog>
 </template>
+
+<style scoped lang="scss">
+.warning-dialog__title {
+  padding: 20px 24px 8px;
+  text-align: center;
+}
+
+.warning-dialog__body {
+  padding: 8px 24px 12px;
+  text-align: center;
+}
+
+.warning-dialog__message {
+  margin: 0 auto 16px;
+  max-width: 100%;
+  color: rgb(var(--v-theme-on-surface));
+  font: inherit;
+  line-height: 1.6;
+  white-space: pre-wrap;
+  overflow-wrap: anywhere;
+}
+
+.warning-dialog__actions {
+  padding: 8px 16px 16px;
+}
+</style>
